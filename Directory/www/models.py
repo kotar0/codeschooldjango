3dc787e484e09f6f6ff1461db0b659aface6d62f
koutaro <koutaro.okuya@gmail.com>
2016-08-19 23:21:39 +0900

    no message

--- Directory/www/models.py
+++ Directory/www/models.py
@@ -6,3 +6,6 @@ class Person(models.Model):
     email = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
